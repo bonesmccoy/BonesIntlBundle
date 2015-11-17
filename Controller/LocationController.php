91ee803114de405eb6d9f829b6d20e2edd725204
Bones McCoy <daniele.murroni@gmail.com>
2015-11-17 21:43:55 +0000

    first tree simple rest route

---
+++ Controller/LocationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Bones\IntlBundle\Controller;
+
+use Bones\IntlBundle\Model\Location\CountryRepository;
+use Bones\IntlBundle\Model\Location\StateRepository;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+
+class LocationController extends FOSRestController
+{
+
+    public function countriesAction()
+    {
+        $countryList = $this->getCountryRepository()->findAll();
+        $view = $this->view($countryList);
+
+        return $this->handleView($view);
+    }
+
+    public function statesAction()
+    {
+        $stateList = $this->getStateRepository()->findAll();
+        $view = $this->view($stateList);
+
+        return $this->handleView($view);
+    }
+
+    public function statesByCountryCodeAction($countryCode)
+    {
+        $stateList = $this->getStateRepository()->findAllByCountryCode($countryCode);
+
+        return $this->handleView($this->view($stateList));
+    }
+
+    /**
+     * @return CountryRepository
+     */
+    private function getCountryRepository()
+    {
+        return $this->get('bones_intl.repository.location.country');
+    }
+
+
+    /**
+     * @return StateRepository
+     */
+    private function getStateRepository()
+    {
+        return $this->get('bones_intl.repository.location.state');
+    }
+
+
+}
+ 
